b82b6137306f3ed19a1e51eba4b8c45f4825105c
Pedro Henrique <pedrosilva10057@gmail.com>
2024-10-08 23:57:17 -0300

    feat: iniciei a estilização da responsividade
    I styled the home section and the navbar

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// new Swiper('.card-wrapper', {
+//     loop: true,
+//     spaceBetween: 30,
+
+//     // Pagination bullets
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//       dynamicBullets: true
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+
+//     // Responsive breakpoints
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1
+//         },
+//         768: {
+//             slidesPerView: 2
+//         },
+//         1024: {
+//             slidesPerView: 3
+//         },
+//     }
+// });
+
+// Swiper settings
 new Swiper('.card-wrapper', {
     loop: true,
     spaceBetween: 30,
@@ -27,4 +59,13 @@ new Swiper('.card-wrapper', {
             slidesPerView: 3
         },
     }
+});
+
+// Navbar toggle for mobile (sidebar)
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+// Toggle 'active' class on click to show/hide the sidebar
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
 });
